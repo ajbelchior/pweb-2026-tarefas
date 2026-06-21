ef1e4cce6049f0a5eedb2e530620e989693bd83a
Ana Júlia <celchior@gmail.com>
2026-06-21 12:33:23 -0300

    Adiciona tarefa04

---
+++ tarefa04/meuapp/views.py
@@ -0,0 +1,13 @@
+from datetime import date
+from django.shortcuts import render
+from .models import Tarefa
+
+def lista_tarefas(request):
+    tarefas = Tarefa.objects.all()
+
+    context = {
+        'tarefas': tarefas,
+        'hoje': date.today()
+    }
+
+    return render(request, 'lista_tarefas.html', context)

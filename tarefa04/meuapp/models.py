97d609eca8f767931375ba2bb390a653bbc08dfe
Ana Júlia <celchior@gmail.com>
2026-06-30 21:49:09 -0300

    Remove pasta tarefa04

--- tarefa04/meuapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-class Tarefa(models.Model):
-    nome = models.CharField(max_length=100)
-    status = models.CharField(max_length=100)
-    prazo = models.DateField()
-
-    def __str__(self):
-        return self.nome
